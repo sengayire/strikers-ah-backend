2f5e17faa5644094bf77e89c4b32ccf35ff54d01
Nkunzi Innocent <innocentnkunzi@yahoo.com>
2019-04-17 21:41:43 +0200

    Feature(article): Get all articles A user should access all articles [Finishes #164489786]

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import articleController from '../../controllers/article';
+import AuthToken from '../../middlewares/validateToken';
 
 const router = express.Router();
 
-router.post('/', articleController.createArticle);
-router.get('/');
+router.post('/', AuthToken.verifyToken, articleController.createArticle);
+// router.get('/:slug', articleController.getArticle);
+router.get('/', articleController.getAllArticles);
 router.delete('/:slug');
 router.put('/:slug');
 
